5c7b84ef3c1b29ef52847126fafe8b52aae7c863
Martyna Bartniczak <aszkee@gmail.com>
2019-07-23 20:53:15 +0200

    css

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React,{Component} from 'react'
 import SearchBar from './SearchBar'
+import VideoList from './VideoList'
+import VideoDetail from './VideoDetail'
 import youtube from '../apis/youtube'
 
 
 class App extends Component {
-    state = {videos : []}
+    state = {videos : [], selectedVideo: null}
+
+componentDidMount() {
+this.handleSubmit('buildings')
+}
 
 handleSubmit = async term => {
     const response = await youtube.get('/search', {
@@ -12,14 +18,29 @@ handleSubmit = async term => {
             q: term
      }
 })
-this.setState({videos: response.data.items})
+this.setState({videos: response.data.items,
+     selectedVideo: response.data.items[0]})
+}
+
+onVideoSelect = video => {
+  this.setState({selectedVideo: video})
 }
 
     render() {
         return(
             <div className="ui container">
                 <SearchBar onSubmit={this.handleSubmit}/>
-                {this.state.videos.length}
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                        <VideoDetail video={this.state.selectedVideo}/>
+                        </div>
+                        <div className="five wide column">
+                        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+                        </div>
+                    </div> 
+                </div>
+                
             </div>
         )
     }
